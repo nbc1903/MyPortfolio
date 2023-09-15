197b330a27d1b0a0d13dd31f44776df87058b985
nbc1903 <nicolas.bolanos97@gmail.com>
2023-09-15 00:24:43 -0500

    fix project images in production

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,9 +1,12 @@
+import PortfolioImage from "/src/assets/portfolio-image.png";
+import GameImage from "/src/assets/game-image.png";
+
 export const projects = [
   {
     title: "CS50 Game Development Final Project",
     description:
       "Final project for the CS50’s Harvard Introduction to Game Development. Game with procedural generation of terrain and an RPG and final boss mechanics.",
-    image: "/src/assets/game-image.JPG",
+    image: GameImage,
     link: "https://www.youtube.com/watch?v=rDrfh1IYnQY",
     tags: ["Unity", "C#"],
   },
@@ -11,7 +14,7 @@ export const projects = [
     title: "Website Portfolio",
     description:
       "Portfolio to keep track of work and projects. Built and designed from scratch. There is more information that will be coming out soon.",
-    image: "/src/assets/portfolio-image.JPG",
+    image: PortfolioImage,
     link: "https://github.com/nbc1903/MyPortfolio",
     tags: [
       "ReactJS",
